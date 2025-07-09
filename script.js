67a4fbbbe0c4861a004da3569df3722d1bdda95c
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-07-09 15:09:14 +0200

    Added functions. not yet completed

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ let container = document.getElementById('cardContainer');
 
 
 let url = 'https://dummyjson.com/products';
+let dataProm = fetchData(url);
+
+async function fetchData(url){
+    let prom = fetch(url);
+    return await prom;
+}
+
+dataProm.then((data)=>{
+    return data.json();
+}).then((dataJ)=>{
+    console.log(dataJ.products); 
+})
+
